6112e03f04e8723f42c6e5ddd63b3d8bfb2484f2
Emilio Morales <elmsterm@gmail.com>
2024-02-22 23:52:15 -0500

    First stage of main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import linkedinLogo from "./assets/linkedin.svg";
+import "./App.css";
+const linkedinURL = "https://www.linkedin.com/in/emilio-morales-798b7522b/";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Emilio Morales</h1>
+            <h2>Computer Science Student</h2>
+            <h3>Virginia Tech 2021-2025</h3>
+            <p className="read-the-docs">Check out my socials</p>
+            <div>
+                <a href={linkedinURL} target="_blank">
+                    <img src={linkedinLogo} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
