d37c67c26d3bc23294d6f30b832e547624536714
Emilio Morales <elmsterm@gmail.com>
2024-02-23 00:41:25 -0500

    fixed asset links

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web-Portfolio/vite-deploy-demo/'
+  base: '/Web-Portfolio/'
 })
