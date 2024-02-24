d0acd19b7a0b75f0715f4fbb9314cfd8f0efaef0
Emilio Morales <elmsterm@gmail.com>
2024-02-24 00:31:18 -0500

    consolidated into one page

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,69 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 }
 
 import "./App.css";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
+import linkedinLogo from "./assets/linkedin.svg";
+import githubLogo from "./assets/github.svg";
+import emailLogo from "./assets/email.svg";
+const linkedinURL = "https://www.linkedin.com/in/emilio-morales-798b7522b/";
+const githubURL = "https://github.com/Elmster401";
+const email = "emorales@vt.edu";
 
 function App() {
     return (
         <>
-            <Header />
+            <header class="header">
+                <div class="title">
+                    <a href="./">Emilio Morales</a>
+                </div>
+                <nav class="navbar">
+                    <ul>
+                        <li>
+                            <a href="./" class="link">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./About" class="link">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./Projects" class="link">
+                                Projects
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./Contact" class="link">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
             <h2>Computer Science Student</h2>
             <h3>Virginia Tech 2021-2025</h3>
-            <Footer />
+            <div class="footer">
+                <div>
+                    <a href={linkedinURL} target="_blank">
+                        <img
+                            src={linkedinLogo}
+                            class="logo"
+                            alt="LinkedId Profile"
+                        />
+                    </a>
+                    <a href={githubURL} target="_blank">
+                        <img
+                            src={githubLogo}
+                            class="logo"
+                            alt="Github Profile"
+                        />
+                    </a>
+                    <a href={"mailto:" + email} target="_blank">
+                        <img src={emailLogo} class="logo" alt="Contact me!" />
+                    </a>
+                </div>
+                <p class="read-the-docs">Emilio Morales 2024</p>
+            </div>
         </>
     );
 }
